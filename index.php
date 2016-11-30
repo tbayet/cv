f15aa0281c3ec3bf1c78183d15e8d038f5651c29
tbayet <tbayet@github.com>
2016-12-01 00:21:34 +0100

    struct

--- index.php
+++ index.php
@@ -10,11 +10,11 @@
 		<source src="http://www.lamoutarde.fr/wp-content/themes/daynnight/videos/working.mp4" type="video/mp4" />
 	</video>
 	<ul class="navbar" id="navbar">
-		<li><a href="#content">Home</a></li>
-		<li><a href="#stud">Studies</a></li>
-		<li><a href="#jobs">Jobs</a></li>
-		<li><a href="#creations">Creations</a></li>
-		<li><a href="#jobs">Jobs</a></li>
+		<li><a href="#timeline">Timeline</a></li>
+		<li><a href="#interest">Centres d'int&eacute;r&ecirc;ts</a></li>
+		<li><a href="#langs">Langues</a></li>
+		<li><a href="#knowledge">Connaissances</a></li>
+		<li><a href="#skills">Comp&eacute;tences</a></li>
 		<li><a href="#contact">Contact</a></li>
 		<li class="icon">
 			<a href="javascript:void(0);" onclick="openNav()">&#9776;</a>
@@ -34,30 +34,145 @@
 	<div id="banner" onscroll="refreshnav()">
 		<div id="content">
 			<div id="navbardiv"></div>
+			<div id="timeline" class="box">
+				<table style="width:100%;"><tr style="width:100%;">
+					<td style="width:10%;">
+						<img id="img06" class="imgtime" src="42.jpg" onclick='deploy("tdtime", "div06");' onmouseover='zoom("img06", 1)' onmouseleave='zoom("img06", 0)' />
+						<img id="img05" class="imgtime" src="guitar.jpg" onclick='deploy("tdtime", "div05");' onmouseover='zoom("img05", 1)' onmouseleave='zoom("img05", 0)' />
+						<img id="img04" class="imgtime" src="webcitrus.jpeg" onclick='deploy("tdtime", "div04");' onmouseover='zoom("img04", 1)' onmouseleave='zoom("img04", 0)' />
+						<img id="img03" class="imgtime" src="ub.jpg" onclick='deploy("tdtime", "div03");' onmouseover='zoom("img03", 1)' onmouseleave='zoom("img03", 0)' />
+						<img id="img02" class="imgtime" src="iut.jpg" onclick='deploy("tdtime", "div02");' onmouseover='zoom("img02", 1)' onmouseleave='zoom("img02", 0)' />
+						<img id="img01" class="imgtime" src="lhf.png" onclick='deploy("tdtime", "div01");' onmouseover='zoom("img01", 1)' onmouseleave='zoom("img01", 0)' />
+					</td>
+					<td id="tdtime" style="border-left-style:solid;">
+						<div id="div01" class="timediv">
+							<h4>2008-2009</h4>
+							<img src="bac.jpg" width="300px" />
+							<p>Obtention du <b>Baccalauréat Scientifique</b> au lycée Hippolyte Fontaine à Dijon.
+							J'y ai choisi de suivre le cursus avec l'option SVT, spécialisation Mathématiques, et un module facultatif d'Art.
+							J'ai étudié le grec ancien et le latin.</p>
+						</div>
+						<div id="div02" class="timediv">
+							<h4>2009-2011</h4>
+							<p>Obtention du <b>Diplôme Universitaire Technologique (DUT) en informatique</b> à l'IUT de Dijon.
+							Le cursus principal initie brièvement aux bases dont l'intéraction matériel-logiciel, le binaire, le C et le Shell.
+							La globalité des cours portent ensuite sur l'algorithmique, l'embarqué, la programmation objet orienté (POO), les bases de données et les diagrammes.
+							En plus de l'informatique, la formation offre une diversité de modules, nous interessant aussi bien au droit et à l'économie 
+							qu'à la communication, au management, à l'anglais ou aux mathématiques.</p>
+							<p>Nous avons réalisé des projets seuls: <i>un Kalaha (jeu de plateau africain) Player.vs.Computer en java</i>, ou en groupe: <i>un moteur 2D de jeux en C#</i>.
+							Le cursus se concluant par un stage de 11 semaines que j'ai effectué au service informatique du Conseil Régionnal de Bourgogne,
+							afin de reprendre le développement d'un programme existant sur l'intranet pour la gestion de projets et de soummettre un cahier des charges visant à remplacer cet outil.</p>
+							<p>J'ai pris part à la vie associative de l'iut en rejoignant l'ETIQ (ETudiant InformatiQue) :
+								<ul>
+									<li>Organisation du local et des activités proposées</li>
+									<li>Organisation d'évènements (intégrations, soirées étudiantes, fêtes de fin d'année)</li>
+									<li>Organisation du festival .dijon//SAITEN</li>
+								</ul>
+							</p>
+						</div>
+						<div id="div03" class="timediv">
+							<h4>2011-2012</h4>
+							<p>Souhaitant améliorer mon niveau d'étude et mes compétences, j'ai rejoins le cursus de la Licence mathématique-informatique à l'Université de Bourgogne.
+							Malheureusement, l'éventail et la qualité des cours m'a terriblement déçu: très peu de lessons d'informatique et un niveau trop bas, donc ennuyant.
+							Je n'ai donc fait qu'un semestre, mais ai pu cependant réaliser quelques projets interessants
+								<ul>
+									<li>Simulation d'un robot aspirateur avec interface graphique en java</li>
+									<li>Modélisation en 3D d'un jeu d'échecs en POV-Ray</li>
+								</ul>
+							</p>
+						</div>
+						<div id="div04" class="timediv">
+							<h4>2012-2013</h4>
+							<p>La personne avec qui j'ai travaillé sur le projet du moteur de jeux 2D m'a proposé de l'utiliser pour développer un jeu de rythme.
+							Nous avons lancé une campagne de financement participatif sur Ulule afin de nous aider à monter une entreprise. La société Webcitrus
+							démarre ainsi son activité en février 2013:
+							<ul>
+								<li>Dépannage et cours d'informatique à domicile (déplacements en vélo)</li>
+								<li>Développement logiciel pour professionnels.</li>
+								<li>Vente/conseils de matériel et montage d'ordinateurs</li>
+								<li>Création du jeu vidéo 8bit the musical</li>
+							</ul></p>
+						</div>
+						<div id="div05" class="timediv">
+							<h4>2014-2015</h4>
+							<p>Jeux-vidéo / Musique / Voyage</p>
+						</div>
+						<div id="div06" class="timediv">
+							<h4>2015-Aujourd'hui</h4>
+							<p>Ecole 42</p>
+						</div>
+					</td>
+				</tr></table>
+			</div>
+			<div id="interest" class="box">
+				<h4>Centres d'int&eacute;r&ecirc;ts</h4>
+				
+			</div>
 			<div class="box">
-				<canvas id="ring" width="100" height="100" onclick='drawcircle("ring", 10, "C");'
-				onmouseover='zoom("ring", 1)' onmouseleave='zoom("ring", 0)'>
-				</canvas>
-				<canvas id="ring2" width="100" height="100" onclick='drawcircle("ring2", 25, "js");'
-				onmouseover='zoom("ring2", 1)' onmouseleave='zoom("ring2", 0)'>
-				</canvas>
-				<canvas id="ring3" width="100" height="100" onclick='drawcircle("ring3", 50, "shell");'
-				onmouseover='zoom("ring3", 1)' onmouseleave='zoom("ring3", 0)'>
-				</canvas>
-				<canvas id="ring4" width="100" height="100" onclick='drawcircle("ring4", 75, "java");'
-				onmouseover='zoom("ring4", 1)' onmouseleave='zoom("ring4", 0)'>
-				</canvas>
+				dfsAAAfsff
 			</div>
-			<div id="creations" class="box">
-				<canvas id="rect" width="300" height="60" onclick='drawrect("rect", 50, "shell");'>
+			<div id="langs" class="box" style="align:center; text-align:center; vertical-align:top;">
+				<h4>Languages</h4>
+				<table style="display:inline-table;border-bottom-style:solid;"><tr>
+					<td>Anglais</td>
+					<td style="border-left-style:solid;">
+						<figure><figcaption>talking</figcaption>
+						<canvas id="bar" width="200" height="15"></canvas></figure>
+						<figure><figcaption>listening</figcaption>
+						<canvas id="bar2" width="200" height="15"></canvas></figure>
+						<figure><figcaption>writing</figcaption>
+						<canvas id="bar3" width="200" height="15"></canvas></figure>
+						<figure><figcaption>reading</figcaption>
+						<canvas id="bar4" width="200" height="15"></canvas></figure>
+					</td>
+				</tr></table>
+				<table style="display:inline-table;border-bottom-style:solid;"><tr>
+					<td>Fran&ccedil;ais</td>
+					<td style="border-left-style:solid;">
+						<figure><figcaption>native</figcaption>
+						<canvas id="bar5" width="200" height="15"></canvas></figure>
+					</td>
+				</tr></table>
+				
 			</div>
-			<div class="box">dfsAAAfsff</div>
-			<div class="box">dfsfsdfsf</div>
-			<div class="box">dfdsfsdfsfs</div>
-			<div class="box">dfsfsdfsf</div>
+			<div id="knowledge" class="box">
+				<h4>Connaissances</h4>
+				<canvas id="ring" width="100" height="100" onclick='drawcircle("ring", 85, "C");'
+				onmouseover='zoom("ring", 1)' onmouseleave='zoom("ring", 0)'></canvas>
+				<canvas id="ring2" width="100" height="100" onclick='drawcircle("ring2", 65, "unix");'
+				onmouseover='zoom("ring2", 1)' onmouseleave='zoom("ring2", 0)'></canvas>
+				<canvas id="ring3" width="100" height="100" onclick='drawcircle("ring3", 90, "html");'
+				onmouseover='zoom("ring3", 1)' onmouseleave='zoom("ring3", 0)'></canvas>
+				<canvas id="ring4" width="100" height="100" onclick='drawcircle("ring4", 55, "css");'
+				onmouseover='zoom("ring4", 1)' onmouseleave='zoom("ring4", 0)'></canvas>
+				<canvas id="ring5" width="100" height="100" onclick='drawcircle("ring5", 75, "js");'
+				onmouseover='zoom("ring5", 1)' onmouseleave='zoom("ring5", 0)'></canvas>
+				<canvas id="ring6" width="100" height="100" onclick='drawcircle("ring6", 30, "php");'
+				onmouseover='zoom("ring6", 1)' onmouseleave='zoom("ring6", 0)'></canvas>
+				<canvas id="ring7" width="100" height="100" onclick='drawcircle("ring7", 20, "java");'
+				onmouseover='zoom("ring7", 1)' onmouseleave='zoom("ring7", 0)'></canvas>
+				<canvas id="ring8" width="100" height="100" onclick='drawcircle("ring8", 30, "c++");'
+				onmouseover='zoom("ring8", 1)' onmouseleave='zoom("ring8", 0)'></canvas>
+				<canvas id="ring9" width="100" height="100" onclick='drawcircle("ring9", 15, "c#");'
+				onmouseover='zoom("ring9", 1)' onmouseleave='zoom("ring9", 0)'></canvas>
+			</div>
+			<div id="skills" class="box">
+				<h4>Comp&eacute;tences</h4>
+				<div>
+				<canvas id="rect" class = "c_rect" width="300" height="60"></canvas>
+				<canvas id="rect2" class = "c_rect" width="300" height="60"></canvas>
+				<canvas id="rect3" class = "c_rect" width="300" height="60"></canvas>
+				<canvas id="rect4" class = "c_rect" width="300" height="60"></canvas>
+				<canvas id="rect5" class = "c_rect" width="300" height="60"></canvas>
+				<canvas id="rect6" class = "c_rect" width="300" height="60"></canvas>
+				<canvas id="rect7" class = "c_rect" width="300" height="60"></canvas>
+				<canvas id="rect8" class = "c_rect" width="300" height="60"></canvas>
+				</div>
+			</div>
 			<div id="contact" class="box">
-			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12475.671361035467!2d2.292143048937684!3d48.902305214780554!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e66f7d03e984ab%3A0xce1d395e61609e94!2sGare+d&#39;Asnieres!5e0!3m2!1sfr!2sfr!4v1480010074851"
-			 width="100%" height="300px" frameborder="0" style="border:0; margin:0; border-radius: 5px;" allowfullscreen></iframe>
+				<h4>Contact</h4>
+				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12475.671361035467!2d2.292143048937684!3d48.902305214780554!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e66f7d03e984ab%3A0xce1d395e61609e94!2sGare+d&#39;Asnieres!5e0!3m2!1sfr!2sfr!4v1480010074851"
+				width="100%" height="300px" frameborder="0" style="border:0; margin:0; border-radius: 5px;" allowfullscreen></iframe>
 			</div>
 		</div>
 	</div>
@@ -79,11 +194,62 @@
 		var profil = document.getElementById("profil");
 		var videofond = document.getElementById("videofond");
 		refreshnav();
-		drawrect("rect", 65, "shell");
-		drawcircle("ring", 10, "C");
-		drawcircle("ring2", 25, "js");
-		drawcircle("ring3", 50, "shell");
-		drawcircle("ring4", 75, "java");
+		simplebar("bar", 25);
+		simplebar("bar2", 70);
+		simplebar("bar3", 60);
+		simplebar("bar4", 90);
+		simplebar("bar5", 99);
+
+		drawrect("rect", 55, "Rigor");
+		drawrect("rect2", 100, "Creativity");
+		drawrect("rect3", 33, "Organization");
+		drawrect("rect4", 70, "Adaptability");
+		drawrect("rect5", 85, "Relational");
+		drawrect("rect6", 67, "Analysis");
+		drawrect("rect7", 60, "Enthusiasm");
+		drawrect("rect8", 93, "Maturity");
+
+		drawcircle("ring", 85, "C");
+		drawcircle("ring2", 65, "unix");
+		drawcircle("ring3", 90, "html");
+		drawcircle("ring4", 55, "css");
+		drawcircle("ring5", 75, "js");
+		drawcircle("ring6", 30, "php");
+		drawcircle("ring7", 20, "java");
+		drawcircle("ring8", 30, "c++");
+		drawcircle("ring9", 15, "c#");
+		
+		function deploy(div, divchild)
+		{
+			var child = document.getElementById(divchild);
+			var parent = document.getElementById(div);
+			var children = parent.children;
+			var i = 0;
+			var inter;
+			var opacity;
+
+			while (i < children.length)
+			{
+				if (children[i].style.display.localeCompare("none") != 0)
+					children[i].style.display = "none";
+				i++;
+			}
+			child.style.opacity = 0;
+			child.style.display = "block";
+			opacity = 0;
+			inter = setInterval(changeOpacity, 100, child);
+
+			function changeOpacity(elem)
+			{
+				elem.style.opacity = opacity;
+				if (opacity > 1)
+				{
+					clearInterval(inter);
+				}
+				opacity += 0.1;
+			}
+		}
+		
 		function openNav() {
 			if (navbar.className === "navbar")
 				navbar.className += " responsive";
--- tools.js
+++ tools.js
@@ -1,13 +1,65 @@
+function simplebar(idcanvas, percent)
+{
+	var canvas = document.getElementById(idcanvas);
+	var context = canvas.getContext("2d");
+	var inter;
+	var i = 1;
+	var	dir = 1;
+
+	percent *= 2;
+	//background
+	context.beginPath();
+	context.lineWidth = 1;
+	context.fillStyle = "#444";
+	context.rect(0, 0, 200, 15);
+	context.closePath();
+	context.fill();
+
+	//borderlines
+	context.beginPath();
+	context.lineWidth = 1;
+	context.strokeStyle = "black";
+	context.rect(0, 0, 300, 15);
+	context.stroke();
+
+	inter = setInterval(animate, 5);
+	function animate()
+	{
+		//percentfill
+		context.beginPath();
+		context.lineWidth = 1;
+		context.fillStyle = "orange";
+		context.rect(1, 1, percent, 13);
+		context.closePath();
+		context.fill();
+		if (i <= 1)
+			dir = 1;
+		else if (i + 5 >= percent)
+			dir = 0;
+		//light
+		context.beginPath();
+		context.lineWidth = 1;
+		context.fillStyle = "yellow";
+		context.rect(i, 1, 5, 13);
+		context.closePath();
+		context.fill();
+		if (dir == 1)
+			i++;
+		else
+			i--;
+	}
+}
+
 function getRandomShade(colorcode)
 {
 	var random;
 	var argb;
 
-	function hexToRgb(hex)
+	function hextoRgb(hexc)
 	{
-		var p = parseInt(hex, 16);
-		var rgb = [(p >> 16) & 255, (p >> 8) & 255, p & 255];
-		return (rgb);
+		var p = parseInt(hexc, 16);
+		var res = [((p >> 16) & 255), ((p >> 8) & 255), (p & 255)];
+		return (res);
 	}
 
 	function rgbtoHex(rgb)
@@ -33,66 +85,65 @@ function drawrect(idcanvas, percent, wordt)
 {
 	var canvas = document.getElementById(idcanvas);
 	var context = canvas.getContext("2d");
+	var inter;
+	var color = "#A45324";
 
+	inter = setInterval(animate, 150);
+	if (percent > 33)
+		color = "#9CAF51";
+	if (percent > 66)
+		color = "#1C9080";
 	percent *= 3;
-
-	//background
-	context.beginPath();
-	context.lineWidth = 1;
-	context.fillStyle = "#1C9080";
-	context.rect(0, 0, percent, 60);
-	context.closePath();
-	context.fill();
-
-	//shadows
-	context.beginPath();
-	context.lineWidth = 5;
-	context.strokeStyle = "#0B614B";
-	context.rect(0, 0, percent - 3, 57);
-	context.stroke();
-
-	//rest background
-	context.beginPath();
-	context.lineWidth = 1;
-	context.fillStyle = "#444";
-	context.rect(percent, 0, 300 - percent, 60);
-	context.closePath();
-	context.fill();
-
-	//rest shadows
-	context.beginPath();
-	context.lineWidth = 5;
-	context.strokeStyle = "black";
-	context.rect(percent + 3, 3, 300 - (percent + 3), 57);
-	context.stroke();
-
-
-	//borderlines
-	context.beginPath();
-	context.lineWidth = 2;
-	context.strokeStyle = "black";
-	context.rect(0, 0, 300, 60);
-	context.stroke();
-
-	animate();
 
 	function animate()
 	{
 		var j;
 
-		for (var i = 1; i < percent; i += 15)
+		//background
+		context.beginPath();
+		context.lineWidth = 1;
+		context.fillStyle = color;
+		context.rect(0, 0, percent, 60);
+		context.closePath();
+		context.fill();
+		for (var i = 0; i < 300; i += 15)
 		{
-			j = Math.round(Math.random() * 4);
-			context.beginPath();
-			context.lineWidth = 1;
-			context.fillStyle = getRandomShade("#1C9080");
-			if (i + 15 > percent)
-				context.rect(i, j * 15 + 1, percent, 60);
-			else
-				context.rect(i, j * 15 + 1, i + 15, 60);
-			context.closePath();
-			context.fill();
-		}
+			for (var k = 0; k < 3; k++)
+			{
+				j = Math.floor(Math.random() * 4);
+				context.beginPath();
+				context.lineWidth = 1;
+				context.fillStyle = getRandomShade(color);
+				context.rect(i, j * 15, 15, 15);
+				context.closePath();
+				context.fill();
+			}
+		}	
+		//rest background
+		context.beginPath();
+		context.lineWidth = 1;
+		context.fillStyle = "#444";
+		context.rect(percent, 0, 300 - percent, 60);
+		context.closePath();
+		context.fill();
+		//rest shadows
+		context.beginPath();
+		context.lineWidth = 5;
+		context.strokeStyle = "black";
+		context.rect(percent + 3, 3, 300 - (percent + 3), 57);
+		context.stroke();
+		//borderlines
+		context.beginPath();
+		context.lineWidth = 2;
+		context.strokeStyle = "black";
+		context.rect(0, 0, 300, 60);
+		context.stroke();
+		//text adding
+		context.fillStyle = "white";
+		context.font = "30px Georgia";
+		context.textBaseline = "bottom";
+		context.textAlign = "left";
+		context.fillText(wordt, 3, 57);
 	}
 }
 
